5abeee2ecc375106ec084b5d8e45c5c5ae0cc618
fl3ixy <lohanderepper@protonmail.com>
2024-12-15 22:12:42 +0100

    Responsive Page 1

--- app/page.jsx
+++ app/page.jsx
@@ -9,55 +9,54 @@ export default function Home() {
     <main className="relative w-auto sm:overflow-hidden">
       <ParticlesBackground />
       <div className="relative flex w-auto flex-col justify-center">
-        <HomePageSection>
-          <div className="flex w-full flex-col">
-            <HomePageArticle
-              type="intro"
-              label="Stage de Lohan"
-              description="Ce site présente la PFMP obligatoire du 23 septembre 2024 au 11 octobre 2024 (3 Semaines) réalisée dans le cadre de la formation en BAC PRO SN RISC. 
+        <HomePageSection isFlexCol={true}>
+          <HomePageArticle
+            type="intro"
+            label="Stage de Lohan"
+            description="Ce site présente la PFMP obligatoire du 23 septembre 2024 au 11 octobre 2024 (3 Semaines) réalisée dans le cadre de la formation en BAC PRO SN RISC. 
                   Ce stage a permis de mettre en pratique les compétences techniques et théoriques acquises durant la formation, 
                   en travaillant sur des problématiques concrètes liées à la gestion et la sécurisation des réseaux au sein de l’entreprise."
-              linkinfos={[
-                {
-                  label: "Présentation",
-                  href: "/Presentation",
-                  className:
-                    "hover:rotate-3 hover:shadow-[-4px_0_8px_5px_rgba(0,114,255,0.8)]",
-                },
-                {
-                  label: "Technique",
-                  href: "/Technique",
-                  className:
-                    "hover:shadow-[4px_0px_8px_5px_rgba(0,114,255,0.8)]",
-                },
-              ]}
-            />
-            <HomePageArticle
-              type="paragraphe"
-              label="Cadre du stage"
-              description="Ce stage, réalisé dans le cadre de la formation en Administration Réseau et Systèmes, a permis de mettre en pratique les compétences théoriques acquises. 
+            linkinfos={[
+              {
+                label: "Présentation",
+                href: "/Presentation",
+                className:
+                  "hover:rotate-3 hover:shadow-[-4px_0_8px_5px_rgba(0,114,255,0.8)]",
+              },
+              {
+                label: "Technique",
+                href: "/Technique",
+                className: "hover:shadow-[4px_0px_8px_5px_rgba(0,114,255,0.8)]",
+              },
+            ]}
+          />
+          <HomePageArticle
+            type="paragraphe"
+            label="Cadre du stage"
+            description="Ce stage, réalisé dans le cadre de la formation en Administration Réseau et Systèmes, a permis de mettre en pratique les compétences théoriques acquises. 
               Cette formation prépare des professionnels capables de gérer et administrer des infrastructures réseau, d’en assurer la sécurité, et de répondre aux besoins techniques des entreprises.
                Les stagiaires y développent des aptitudes essentielles en connectivité, en protection des données et en résolution des problématiques informatiques, 
               renforçant ainsi leur expertise dans un domaine clé pour la performance des organisations modernes."
-            />
-            {/* TABLE RIGHT */}
-            <HomePageArticle
-              type="paragraphe"
-              label="Cadre du stage"
-              description="Ce stage, réalisé dans le cadre de la formation en Administration Réseau et Systèmes, a permis de mettre en pratique les compétences théoriques acquises. 
+          />
+          <HomePageArticle
+            type="paragraphe"
+            label="Cadre du stage"
+            description="Ce stage, réalisé dans le cadre de la formation en Administration Réseau et Systèmes, a permis de mettre en pratique les compétences théoriques acquises. 
               Cette formation prépare des professionnels capables de gérer et administrer des infrastructures réseau, d’en assurer la sécurité, et de répondre aux besoins techniques des entreprises.
-               Les stagiaires y développent des aptitudes essentielles en connectivité, en protection des données et en résolution des problématiques informatiques, 
+              Les stagiaires y développent des aptitudes essentielles en connectivité, en protection des données et en résolution des problématiques informatiques, 
               renforçant ainsi leur expertise dans un domaine clé pour la performance des organisations modernes."
-            />
+          />
+        </HomePageSection>
 
-            <HomePageSection>
-              <>
-                <HomePageTable
-                  label="Service de dépannage"
-                  subLabel="CHRSO"
-                  subject="3 Techniciens Réseaux"
-                  subjectDescription="PRÉSENTATION DU SERVICE"
-                  description="LypsChill est une plateforme de streaming qui fonctionne de manière
+        {/* TABLE RIGHT */}
+        <HomePageSection>
+          <>
+            <HomePageTable
+              label="Service de dépannage"
+              subLabel="CHRSO"
+              subject="3 Techniciens Réseaux"
+              subjectDescription="PRÉSENTATION DU SERVICE"
+              description="LypsChill est une plateforme de streaming qui fonctionne de manière
                   similaire à Netflix, permettant aux utilisateurs de regarder un
                   large éventail de films, de séries, de documentaires et d'autres
                   contenus sans interruption publicitaire. Disponible sur divers
@@ -67,33 +66,34 @@ export default function Home() {
                   habitudes de visionnage. En outre, la plateforme propose des
                   productions originales de grande qualité, ce qui contribue à
                   renforcer son impact."
-                />
-                <Image
-                  className="z-20 -rotate-6 transform rounded-3xl shadow-lg transition-all duration-500 ease-in-out hover:translate-x-5 hover:translate-y-2 hover:rotate-6 hover:scale-110 hover:opacity-90 hover:shadow-2xl hover:shadow-blue-500/40 sm:mx-auto sm:w-auto md:flex md:w-auto md:items-center md:justify-center lg:mx-auto lg:mt-4 lg:w-[500px] lg:translate-x-14 lg:rotate-3 lg:transition lg:duration-200 lg:ease-in-out lg:hover:rotate-12"
-                  src={"/img/CHlogo.png"}
-                  alt="hotelimg"
-                  width={"250"}
-                  height={"250"}
-                />
-              </>
-            </HomePageSection>
-            <HomePageArticle
-              type="paragraphe"
-              label="Le Centre Hospitalier"
-              description="Le Centre Hospitalier de la Région de Saint-Omer (CHRSO) est une institution regroupant 1 504 agents, dont 130 médecins et 975 soignants environ. Réparti en plusieurs pavillons spécialisés, 
+            />
+            <Image
+              className="z-20 -rotate-6 transform rounded-3xl shadow-lg transition-all duration-500 ease-in-out hover:translate-x-5 hover:translate-y-2 hover:rotate-6 hover:scale-110 hover:opacity-90 hover:shadow-2xl hover:shadow-blue-500/40 sm:mx-auto sm:w-auto md:flex md:w-auto md:items-center md:justify-center lg:mx-auto lg:mt-4 lg:w-[500px] lg:translate-x-14 lg:rotate-3 lg:transition lg:duration-200 lg:ease-in-out lg:hover:rotate-12"
+              src={"/img/CHlogo.png"}
+              alt="hotelimg"
+              width={"250"}
+              height={"250"}
+            />
+          </>
+        </HomePageSection>
+        
+        <HomePageSection>
+          <HomePageArticle
+            type="paragraphe"
+            label="Le Centre Hospitalier"
+            description="Le Centre Hospitalier de la Région de Saint-Omer (CHRSO) est une institution regroupant 1 504 agents, dont 130 médecins et 975 soignants environ. Réparti en plusieurs pavillons spécialisés, 
               il propose une large gamme de services médicaux. Mon stage s’est déroulé au sein du service informatique, un élément clé du bon fonctionnement de l’hôpital. Ce service est responsable de la gestion des systèmes d’information, 
               de la maintenance du matériel informatique et de la sécurité des données. 
               Il joue un rôle central dans la coordination entre les différents services et garantit la continuité des soins grâce à des outils numériques performants et sécurisés."
-            />
+          />
 
-            <HomePageArticle
-              type="paragraphe"
-              label="Le Service Informatique"
-              description="Le service informatique du Centre Hospitalier de la Région de Saint-Omer (CHRSO) est composé de 10 employés aux rôles spécifiques : 3 sont spécialisés en gestion et résolution des problèmes applicatifs liés aux logiciels médicaux et administratifs, 
+          <HomePageArticle
+            type="paragraphe"
+            label="Le Service Informatique"
+            description="Le service informatique du Centre Hospitalier de la Région de Saint-Omer (CHRSO) est composé de 10 employés aux rôles spécifiques : 3 sont spécialisés en gestion et résolution des problèmes applicatifs liés aux logiciels médicaux et administratifs, 
               3 se consacrent à la maintenance et à la configuration du matériel informatique, 1 administrateur système assure la stabilité des serveurs et la sécurité des données, 1 administrateur réseaux veille à la connectivité et à la sécurisation des échanges, 
               tandis que le responsable informatique coordonne l’ensemble des activités et garantit l’alignement des ressources sur les objectifs stratégiques de l’établissement."
-            />
-          </div>
+          />
         </HomePageSection>
 
         {/* PARAGRAPHE 2 */}
--- components/HomePage/HomePageSection.jsx
+++ components/HomePage/HomePageSection.jsx
@@ -1,11 +1,20 @@
 "use client";
 
 import React from "react";
+import clsx from "clsx";
 
-export default function HomePageSection({ children }) {
+export default function HomePageSection({ children, isFlexCol }) {
   return (
     <section className="flex justify-center sm:text-center">
-      <div className="my-12 flex w-[1300px] sm:w-[520px] justify-center text-9xl text-white sm:mx-0 sm:mb-14 sm:flex-col sm:gap-5 sm:text-9xl md:mx-16 md:flex-col md:gap-5">
+      <div
+        className={clsx(
+          "my-12 flex w-[1300px] justify-center text-9xl text-white sm:mx-0 sm:mb-14 sm:w-[520px] sm:flex-col sm:gap-5 sm:text-9xl md:mx-16 md:flex-col md:gap-5",
+          {
+            "flex-row": !isFlexCol,
+            "flex-col": isFlexCol,
+          },
+        )}
+      >
         {children}
       </div>
     </section>
